e8e4f2104edf48ad0449df189cd60b52e31ae11f
syakaidane <121926213+syakaidane@users.noreply.github.com>
2023-04-16 07:58:17 +0000

    chapter1

---
+++ chapter1/Age.java
@@ -0,0 +1,9 @@
+public class Age {
+  public static void main (String[] args) {
+    int age;
+    age = 30;
+    System.out.println("私の年齢は" + age);
+    age = 31;
+    System.out.println("私の本当の年齢は" + age);
+  }
+}
---
+++ chapter1/Pi.java
@@ -0,0 +1,11 @@
+public class Pi {
+  public static void main(String[] args) {
+    final double pi = 3.14;
+    int pie = 5;
+    System.out.println("半径" + pie + "cmのパイの面積は、");
+    System.out.println(pie * pie * pi);
+    System.out.println("パイの半径を倍にすると、");
+    pie = 10;
+    System.out.println(pie * pie * pi);
+  }
+}
