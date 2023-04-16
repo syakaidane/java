0dce295a229f7103d985e8e330f8aa31aa74edd2
syakaidane <121926213+syakaidane@users.noreply.github.com>
2023-04-16 07:41:32 +0000

    chapter0

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
 	public static void main(String[] args) { 
-		System.out.println("hello, world!");
+		System.out.println("Hello, World!");
+		System.out.println("31 + 31 の計算をします。");
+		System.out.println(31 + 31);
+		int x;
+		x = 6;
+		System.out.println(x * x * 3.14);
 	}
 }
