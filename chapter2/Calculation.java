b8401f197a4b36d57e2bc052d5fadeb04fb072df
syakaidane <121926213+syakaidane@users.noreply.github.com>
2023-04-16 08:17:22 +0000

    chapter2-1

---
+++ chapter2/Calculation.java
@@ -0,0 +1,17 @@
+public class Calculation {
+  public static void main (String[] args) {
+    int a;
+    int b;
+    a = 20;
+    b = a + 5;
+    System.out.println(a);
+    System.out.println(b);
+    
+    System.out.println("私の好きな記号は二重引用符（\"）です");
+    
+    int c;
+    c = 100;
+    c++;
+    System.out.println(c);
+  }
+}
---
+++ chapter2/Type.java
@@ -0,0 +1,17 @@
+public class Type {
+  public static void main (String[] args) {
+    float f = 3;
+    //double d = f;
+    System.out.println(f);
+    //System.out.println(d);
+    
+    double d = 8.5 / 2;
+    long l  = 5 + 2L;
+    System.out.println(d);
+    System.out.println(l);
+    
+    String msg = "私の年齢は" + 23;
+    System.out.println(msg);
+    
+  }
+}
