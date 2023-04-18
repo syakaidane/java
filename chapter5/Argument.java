9cf7e516f0f920ed13934d8184008e039db28e46
syakaidane <121926213+syakaidane@users.noreply.github.com>
2023-04-18 06:19:55 +0000

    chapter5-1

---
+++ chapter5/Argument.java
@@ -0,0 +1,13 @@
+public class Argument {
+  // int型配列を受け取り、全ての要素を表示するメソッド
+  public static void printArray(int[] array) {
+    for (int element : array) {
+      System.out.println(element);
+    }
+  }
+  
+  public static void main(String[] args) {
+    int[] array = {1, 2, 3};
+    printArray(array);   //配列を渡す
+  }
+}
