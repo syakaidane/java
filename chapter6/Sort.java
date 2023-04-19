9c9ad2e64b0fbb36c2568447c99f6426e19c2a5c
syakaidane <121926213+syakaidane@users.noreply.github.com>
2023-04-19 07:32:28 +0000

    chapter6

---
+++ chapter6/Sort.java
@@ -0,0 +1,9 @@
+public class Sort {
+  public static void main(String[] args) {
+    int[] height = {172, 149, 152, 191, 155};
+    java.util.Arrays.sort(heights);
+    for (int h : heights) {
+      System.out.println(h);
+    }
+  }
+}
---
+++ chapter6/komon/main/Main.java
@@ -0,0 +1,11 @@
+import komon.Zenhan.comment.Zenhan;
+import komon.Kouhan.comment.Kouhan;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    doWarusa();
+    doTogame();
+    callDeae();
+    showMondokoro();
+  }
+}
