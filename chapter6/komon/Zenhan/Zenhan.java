c90614faa605ce5458118c24878d20d2b12d78bc
syakaidane <121926213+syakaidane@users.noreply.github.com>
2023-04-21 10:11:35 +0000

    chapter7

--- chapter6/komon/Zenhan/Zenhan.java
+++ chapter6/komon/Zenhan/Zenhan.java
@@ -4,7 +4,6 @@ public class Zenhan {
   public static void doWarusa() {
     System.out.println("きなこでござる。食えませんがの。");
   }
-  
   public static void doTogame() {
     System.out.println("この老いぼれの目はごまかせませんぞ。");
   }
