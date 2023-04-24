e0c3cf46d52c964af5e7e71a03ba00e198214b38
syakaidane <121926213+syakaidane@users.noreply.github.com>
2023-04-24 09:41:59 +0000

    chapter8

--- App/Hero.java
+++ App/Hero.java
@@ -3,7 +3,7 @@ public class Hero {
   String name;  // 名前の宣言
   int hp;       // HPの宣言
   
-  void sleep() {
+  public void sleep() {
     this.hp = 100;
     System.out.println(this.name + "は、眠って回復した！");
   }
