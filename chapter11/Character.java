90b3188c296af208b3148266df68581794aeb674
syakaidane <121926213+syakaidane@users.noreply.github.com>
2023-05-05 11:06:42 +0000

    chapter11-2

--- chapter11/Character.java
+++ chapter11/Character.java
@@ -1,9 +1,8 @@
-public class Character {
+public abstract class Character {
   String name;
   int hp;
   public void run() {
     System.out.println(this.name + "は逃げ出した");
   }
-  public void attack(Matango m) {
-  }
+  public abstract void attack(Matango m);
 }
