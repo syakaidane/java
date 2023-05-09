385f9f20bc147727e6d46686b5f964e807bb3067
syakaidane <121926213+syakaidane@users.noreply.github.com>
2023-05-09 10:37:37 +0000

    ALH

--- test/ALH.java
+++ test/ALH.java
@@ -1,7 +1,14 @@
 public class ALH {
   public static void main(String[] args) {
-    for(int i = 0;i <= 100;i++) {
-      System.out.println(i);
+    for(int i = 1;i <= 100;i++) {
+      if (i % 15 == 0) {
+        System.out.println("FizzBuzz");
+      } else if (i % 3 == 0) {
+        System.out.println("Fizz");
+      } else if (i % 5 == 0) {
+        System.out.println("Buzz");
+      } else {
+      }
     }
   }
 }
